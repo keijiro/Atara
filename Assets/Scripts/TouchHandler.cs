185b49550a4511648a1c1554d9d84eaa9687bb98
Keijiro Takahashi <keijiro@gmail.com>
2023-12-19 17:04:13 +0900

    Touch position

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -14,11 +14,18 @@ public sealed class TouchHandler : MonoBehaviour
 
     bool IsTouchOn => Input.GetMouseButton(0) || ForceTouch;
 
+    Vector2 NormalizedMousePosition
+      => new Vector2(Input.mousePosition.x / Screen.width,
+                     Input.mousePosition.y / Screen.height);
+
+    VisualEffect[] _vfxList;
+
     #endregion
 
     #region Predefined constants
 
     static readonly int ID_Touch = Shader.PropertyToID("Touch");
+    static readonly int ID_TouchPosition = Shader.PropertyToID("TouchPosition");
 
     #endregion
 
@@ -26,13 +33,13 @@ public sealed class TouchHandler : MonoBehaviour
 
     IEnumerator Start()
     {
-        var vfxList = GetComponentsInChildren<VisualEffect>();
+        _vfxList = GetComponentsInChildren<VisualEffect>();
 
         while (true)
         {
             while (!IsTouchOn) yield return null;
 
-            foreach (var vfx in vfxList)
+            foreach (var vfx in _vfxList)
             {
                 if (vfx.HasBool(ID_Touch)) vfx.SetBool(ID_Touch, true);
                 vfx.Play();
@@ -42,7 +49,7 @@ public sealed class TouchHandler : MonoBehaviour
 
             while (IsTouchOn) yield return null;
 
-            foreach (var vfx in vfxList)
+            foreach (var vfx in _vfxList)
             {
                 if (vfx.HasBool(ID_Touch)) vfx.SetBool(ID_Touch, false);
                 vfx.Stop();
@@ -50,6 +57,13 @@ public sealed class TouchHandler : MonoBehaviour
 
             yield return null;
         }
+    }
+
+    void Update()
+    {
+        foreach (var vfx in _vfxList)
+            if (vfx.HasVector2(ID_TouchPosition))
+                vfx.SetVector2(ID_TouchPosition, NormalizedMousePosition);
     }
 
     #endregion
