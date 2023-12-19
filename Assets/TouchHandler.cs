ef8fc2301ff9b58e4aee93985ffeb1d905e8244b
Keijiro Takahashi <keijiro@gmail.com>
2023-12-19 11:32:20 +0900

    Touch handling

---
+++ Assets/TouchHandler.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.VFX;
+using IEnumerator = System.Collections.IEnumerator;
+
+public sealed class TouchHandler : MonoBehaviour
+{
+    #region Public properties
+
+    public bool ForceTouch { get; set; }
+
+    #endregion
+
+    #region Private members
+
+    bool IsTouchOn => Input.GetKey(KeyCode.Space) || ForceTouch;
+
+    #endregion
+
+    #region Predefined constants
+
+    static readonly int ID_Touch = Shader.PropertyToID("Touch");
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    IEnumerator Start()
+    {
+        var vfxList = GetComponentsInChildren<VisualEffect>();
+
+        while (true)
+        {
+            while (!IsTouchOn) yield return null;
+
+            foreach (var vfx in vfxList)
+            {
+                if (vfx.HasBool(ID_Touch)) vfx.SetBool(ID_Touch, true);
+                vfx.Play();
+            }
+
+            yield return null;
+
+            while (IsTouchOn) yield return null;
+
+            foreach (var vfx in vfxList)
+            {
+                if (vfx.HasBool(ID_Touch)) vfx.SetBool(ID_Touch, false);
+                vfx.Stop();
+            }
+
+            yield return null;
+        }
+    }
+
+    #endregion
+}
