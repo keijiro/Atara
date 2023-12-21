44d9ca90b41b29d6747972df1c13e0dd594863d4
Keijiro Takahashi <keijiro@gmail.com>
2023-12-21 14:35:23 +0900

    Camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,26 +1,51 @@
+using Klak.Math;
 using UnityEngine;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
 
 public sealed class CameraController : MonoBehaviour
 {
     #region Public properties
 
-    [field:SerializeField] public float PanWidth { get; set; } = 90;
+    [field:SerializeField] public float3 BaseAngles { get; set; } = 45;
+    [field:SerializeField] public float PanWidth { get; set; } = 180;
+    [field:SerializeField] public float TweenSpeed { get; set; } = 10;
+    [field:SerializeField] public uint RandomSeed { get; set; } = 1234;
 
     #endregion
 
+    #region Private members
+
+    Random _random;
+    quaternion _rotation;
+
+    float MousePosition
+      => Input.mousePosition.x / Screen.width - 0.5f;
+
+    quaternion NextRandomRotation
+      => quaternion.EulerZXY(_random.NextFloat3(-1, 1) * BaseAngles);
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+      => (_random, _rotation) = (new Random(RandomSeed), quaternion.identity);
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            transform.parent.localRotation =
-                Quaternion.Euler(Random.Range(-45, 45),
-                                 Random.Range(-45, 45),
-                                 Random.Range(-90, 90));
+        var xform1 = transform.parent.parent;
+        var xform2 = transform.parent;
+
+        if (Input.GetMouseButtonDown(0)) _rotation = NextRandomRotation;
 
         if (Input.GetMouseButton(0))
-        {
-            var x = (float)Input.mousePosition.x / Screen.width;
-            transform.parent.parent.localRotation =
-              Quaternion.AngleAxis((x - 0.5f) * PanWidth, Vector3.up);
-        }
+            xform1.localRotation =
+              Quaternion.AngleAxis(MousePosition * PanWidth, Vector3.up);
+
+        xform2.localRotation =
+            ExpTween.Step(xform2.localRotation, _rotation, TweenSpeed);
     }
+
+    #endregion
 }
